852d1debd141fe13e78a047011efbfeddbd030b3
sonam kumari <sonam20@navgurukul.org>
2023-10-05 15:23:35 +0530

    added navgurukul imges in navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from './Components/Home';
 import Contect from './Components/Contect';
 import About from './Components/About';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import navgurukul from "./navgurukul.png";
+
 
 
 function App() {
@@ -14,6 +16,10 @@ function App() {
         <Link className='about' to="/about">About</Link>
         <Link className='Contect' to="/Contect">Contect</Link>
 
+        <a href="https://www.navgurukul.org">
+          <img className="images" src={navgurukul} alt="Navgurukul-Logo" />
+        </a>
+        
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
