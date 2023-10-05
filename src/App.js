2fb4a6d0f165510c5dbfb3b6aac911bf74eafd40
sonam kumari <sonam20@navgurukul.org>
2023-10-05 15:02:53 +0530

    Routing in navigation

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import React from 'react';
 import Home from './Components/Home';
 import Contect from './Components/Contect';
 import About from './Components/About';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <>
-      <Home />
-      <About />
-      <Contect />
-    </>
+    <Router>
+      <div className='nav'>
+        <Link className='home' to="/">Home</Link>
+        <Link className='about' to="/about">About</Link>
+        <Link className='Contect' to="/Contect">Contect</Link>
+
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Contect" element={<Contect />} />
+      </Routes>
+    </Router>
+
+
   );
 }
 
 export default App;
+
+
