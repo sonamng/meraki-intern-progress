868ae1fb35ffd07d1f826d8dfcccb602fe572689
sonam kumari <sonam20@navgurukul.org>
2023-10-06 16:10:00 +0530

    create two column using mui

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,16 @@
 import { Typography } from "@mui/material";
-import { AccordionDetails } from '@mui/material';
-import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
 
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+
   color: theme.palette.text.secondary,
 }));
 
@@ -19,29 +19,22 @@ const Item = styled(Paper)(({ theme }) => ({
 function Home() {
   return (
     <>
-      
-      <Typography variant="h3">My Home-Toun </Typography>
-      <AccordionDetails> I ................ </AccordionDetails>
-
       <Box sx={{ width: '100%' }}>
-      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid item xs={6}>
-          <Item>1</Item>
-        </Grid>
-        <Grid item xs={6}>
-          <Item>2</Item>
-        </Grid>
-        <Grid item xs={6}>
-          <Item>3</Item>
-        </Grid>
-        <Grid item xs={6}>
-          <Item>4</Item>
-        </Grid>
-      </Grid>
-    </Box>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={6}>
+            <Item>
+              <Typography variant="h3"> My Hometown </Typography>
+            </Item>
+          </Grid>
 
+          <Grid item xs={6}>
+            <Item>
+              <Typography variant="h3"> About Hometown </Typography>
 
-
+            </Item>
+          </Grid>
+        </Grid>
+      </Box>
     </>
   );
 }
