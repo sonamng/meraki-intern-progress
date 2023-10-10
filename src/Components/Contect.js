93595a75f884ab36a1a02c749d0e4ffa238931fe
sonam kumari <sonam20@navgurukul.org>
2023-10-10 21:44:51 +0530

    added lodding button

--- src/Components/Contect.js
+++ src/Components/Contect.js
@@ -12,75 +12,81 @@ import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
 import SvgIcon from '@mui/material/SvgIcon';
 import Chip from '@mui/material/Chip';
+import CircularProgress from '@mui/material/CircularProgress';
 
 
 
 
 
-  const style = {
-    width: '100%',
-    maxWidth: 360,
-    bgcolor: 'background.paper',
-  };
 
-  function Contect() {
-    return (
-      <>
+const style = {
+  width: '100%',
+  maxWidth: 360,
+  bgcolor: 'background.paper',
+};
 
-        <List sx={style} component="nav" aria-label="mailbox folders">
-          <ListItem >
-            <ListItemText primary="Inbox" />
-          </ListItem>
-          <Divider />
-          <ListItem divider>
-            <ListItemText primary="Drafts" />
-          </ListItem>
-          <ListItem >
-            <ListItemText primary="Trash" />
-          </ListItem>
-          <Divider light />
-          <ListItem >
-            <ListItemText primary="Spam" />
-          </ListItem>
-        </List>
+function Contect() {
+  return (
+    <>
 
-        <Stack direction="row" spacing={1}>
-          <Chip label="Clickable"  />
-          <Chip label="Clickable" variant="outlined"  />
+      <List sx={style} component="nav" aria-label="mailbox folders">
+        <ListItem >
+          <ListItemText primary="Inbox" />
+        </ListItem>
+        <Divider />
+        <ListItem divider>
+          <ListItemText primary="Drafts" />
+        </ListItem>
+        <ListItem >
+          <ListItemText primary="Trash" />
+        </ListItem>
+        <Divider light />
+        <ListItem >
+          <ListItemText primary="Spam" />
+        </ListItem>
+      </List>
+
+      <Stack direction="row" spacing={1}>
+        <Chip label="Clickable" />
+        <Chip label="Clickable" variant="outlined" />
+      </Stack>
+      <SvgIcon>
+        <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+      </SvgIcon>
+
+      <Box sx={{ display: 'flex' }}>
+        <CircularProgress />
+      </Box>
+      <Typography variant='h4' style={{ textAlign: "center" }}>Contect Me</Typography>
+
+
+      <Box sx={{ width: '100%' }}>
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"        >
+          <FormControl>
+            <FormLabel>Name</FormLabel>
+            <TextField placeholder='Name'></TextField>
+            <FormLabel> Last Name</FormLabel>
+            <TextField placeholder='Lastname'></TextField>
+            <FormLabel>Email</FormLabel>
+            <TextField placeholder='Email'></TextField>
+            <FormLabel>Phone</FormLabel>
+            <TextField placeholder='Phone'></TextField>
+
+            <Button>Submit</Button>
+          </FormControl>
+        </Grid>
+
+        <Stack style={{ display: "flex", justifyContent: "center", padding: "20px" }} direction="row" spacing={2}>
+          <Button variant="outlined" >Contect</Button>
+          <Button variant="outlined" >Email</Button>
         </Stack>
-        <SvgIcon>
-          <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-        </SvgIcon>
-        <Typography variant='h4' style={{ textAlign: "center" }}>Contect Me</Typography>
+      </Box>
 
+    </>
+  );
+}
 
-        <Box sx={{ width: '100%' }}>
-          <Grid
-            container
-            justifyContent="center"
-            alignItems="center"        >
-            <FormControl>
-              <FormLabel>Name</FormLabel>
-              <TextField placeholder='Name'></TextField>
-              <FormLabel> Last Name</FormLabel>
-              <TextField placeholder='Lastname'></TextField>
-              <FormLabel>Email</FormLabel>
-              <TextField placeholder='Email'></TextField>
-              <FormLabel>Phone</FormLabel>
-              <TextField placeholder='Phone'></TextField>
-
-              <Button>Submit</Button>
-            </FormControl>
-          </Grid>
-
-          <Stack style={{ display: "flex", justifyContent: "center", padding: "20px" }} direction="row" spacing={2}>
-            <Button variant="outlined" >Contect</Button>
-            <Button variant="outlined" >Email</Button>
-          </Stack>
-        </Box>
-
-      </>
-    );
-  }
-
-  export default Contect;
+export default Contect;
